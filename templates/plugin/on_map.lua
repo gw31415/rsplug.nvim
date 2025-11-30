4a935593b97fa06e2151e0b5f919c39f0253c9f5
gw31415 <24710985+gw31415@users.noreply.github.com>
2025-11-30 21:11:41 +0900

    Revert "fix: Add workaround for op-pending mode"
    This reverts commit 99fa603f430e5f25b9a4f76aa18fbcbd7b413c72.

--- templates/plugin/on_map.lua
+++ templates/plugin/on_map.lua
@@ -5,10 +5,6 @@ local ns_id = nil
 ns_id = vim.on_key(function(_key, typed)
 	vim.on_key(nil, ns_id)
 	require '_rsplug/on_map'.setup 'n'
-
-	-- Workaround for operator-pending mode not being detected properly
-	require '_rsplug/on_map'.setup 'o'
-
 	vim.schedule(function()
 		vim.api.nvim_feedkeys(typed, 'm', true)
 	end)
